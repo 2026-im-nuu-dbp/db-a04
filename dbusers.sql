3ee53a2f65ead71566ba2eabaf48d65eb292b403
Su Wenxuan <u1333004@o365.nuu.edu.tw>
2026-04-14 11:07:59 +0800

    註冊

--- dbusers.sql
+++ dbusers.sql
@@ -1,1 +1,9 @@
 // Create a new table called "dbusers"
+
+CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    nickname VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    gender VARCHAR(10),
+    interest VARCHAR(255)
+);
---
+++ public/register.php
@@ -0,0 +1,27 @@
+<?php
+$usrename = $_POST['username'];
+$nickname = $_POST['nickname'];
+$password = $_POST['password'];
+$gender = $_POST['gender'];
+$interest = $_POST['interest'];
+
+$servername="localhost";
+$dbusername="root";
+$dbpassword="";
+$dbname="db-a04";
+
+$conn=new mysqli($servername,$dbusername,$dbpassword,$dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connection successful!";
+}
+
+$sql = "INSERT INTO users (username, nickname, password, gender, interest) VALUES ('$usrename', '$nickname', '$password', '$gender', '$interest')";
+if ($conn->query($sql) === TRUE) {
+    echo "註冊成功！";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
